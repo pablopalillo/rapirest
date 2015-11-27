44b37f9d7735c367020e343518fb63d302697f27
Pablo Martínez <pablo.576@hotmail.com>
2015-11-27 15:57:37 +0000

    servicio ajax a medias

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-$(document).foundation();
+$(document).ready(function(){
+    $("#search").click(function(){
+       
+       $.ajax({
+        method: "GET",
+        url: "https://4erer.jelastic.servint.net/jogo/rest/product/getallproducts",
+        dataType: "text"
+        })
+        .done(function(data){
+            console.log(data);
+           /** data.each(function() {
+                    console.log( $( this ) );
+              }); **/    
+        })
+        
+    });
+});
