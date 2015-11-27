cc4a7cbac4aa7895d4d7b5943c96864870a4cc2a
pablopalillo <pablo.576@hotmail.com>
2015-11-27 12:08:00 -0500

    Complete

--- js/app.js
+++ js/app.js
@@ -1,17 +1,45 @@
 $(document).ready(function(){
     $("#search").click(function(){
-       
+      $("#table-1 tbody").html("");
+
        $.ajax({
         method: "GET",
-        url: "https://4erer.jelastic.servint.net/jogo/rest/product/getallproducts",
-        dataType: "text"
+        url: "/jogo/rest/product/getallproducts",
+        dataType: "json",
+        beforeSend: function(){
+                      $("#search").attr("class","disabled button imput-rapi-button");
+                      $("#search").val("Loading");
+                    }
         })
+
+        .fail(function(){
+
+          alert("Error");
+
+        })
+
         .done(function(data){
-            console.log(data);
-           /** data.each(function() {
-                    console.log( $( this ) );
-              }); **/    
+
+          $("#table-1").show();
+          $.each(data, function(i, item)
+           {
+              $("#table-1 tbody").append("<tr>");
+              $("#table-1 tbody").append("<td>"+item.code+"</td>");
+              $("#table-1 tbody").append("<td>"+item.name+"</td>");
+              $("#table-1 tbody").append("<td>"+item.cost+"</td>");
+              $("#table-1 tbody").append("</td>");
+              $("#table-1 tbody").append("<tr>");
+
+           });
+
         })
-        
+
+        .always(function(data){
+
+          $("#search").attr("class","success button imput-rapi-button");
+          $("#search").val("Play");
+
+        })
+
     });
 });
